a11d47a7e55b83f87c96ff638db740f44a5b45df
tubapablo <32420631+tubapablo@users.noreply.github.com>
2017-11-29 10:46:45 -0500

    Update snake.py
    Directions

--- snake.py
+++ snake.py
@@ -48,6 +48,10 @@ def get_direction(previous_direction, event_key):
         return DIRECTION_LEFT
     elif event_key == pygame.K_UP:
         return DIRECTION_UP
+    elif event_key == pygame.K_DOWN:
+        return DIRECTION_DOWN
+    elif event_key == pygame.K_RIGHT:
+        return DIRECTION_RIGHT
     return previous_direction
 
 def create_food_position():

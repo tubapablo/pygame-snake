90e3db851cf73bdd6121ff771727b99851cf64fd
tubapablo <32420631+tubapablo@users.noreply.github.com>
2017-12-10 15:20:40 -0500

    Partial snake_ran_out_of_bounds

--- snake.py
+++ snake.py
@@ -79,6 +79,10 @@ def snake_ran_out_of_bounds(snake):
     snake - list of 2-tuples representing the positions of each snake segment
     Note that the grid is GRID_WIDTH cells wide and GRID_HEIGHT cells high.
     """
+    if snake[0] < (0, GRID_WIDTH -1):
+        return True
+    if snake[0] > (29, GRID_WIDTH -1):
+        return True
     return False
 
 def snake_intersected_body(snake):
